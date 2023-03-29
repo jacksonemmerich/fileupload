fd206d1aad9dbe144ad9a04cf820c21fe843084a
Jackson Emmerich <jacksonemmerich@gmail.com>
2023-03-28 23:17:50 -0400

    Created service, repository, and class for entities

---
+++ src/main/java/prefeitura/pvh/fileupload/repository/StorageRepository.java
@@ -0,0 +1,13 @@
+package prefeitura.pvh.fileupload.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import prefeitura.pvh.fileupload.entity.ImageData;
+
+import java.util.Optional;
+
+public interface StorageRepository extends JpaRepository<ImageData, Long> {
+
+
+    Optional<ImageData> findByName(String fileName);
+
+}
---
+++ src/main/java/prefeitura/pvh/fileupload/service/StorageService.java
@@ -0,0 +1,14 @@
+package prefeitura.pvh.fileupload.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import prefeitura.pvh.fileupload.repository.StorageRepository;
+
+@Service
+public class StorageService {
+    @Autowired
+    private StorageRepository storageRepository;
+
+    //https://www.youtube.com/watch?v=XUL60-Ke-L8
+    // 7:42
+}
